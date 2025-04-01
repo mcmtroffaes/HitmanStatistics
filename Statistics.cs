1e49493fc34822b2a1167c0a7b90e9f46abfe765
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2025-04-01 19:47:31 +0100

    Fix whitespace.

--- Statistics.cs
+++ Statistics.cs
@@ -4,66 +4,66 @@
     // All the possible Silent Assassin combinations for Hitman 2
     // https://docs.google.com/spreadsheets/d/1i6dmzcBROqoJlsQjUGY8wxdqwxt2hXzjB9fPVggTf2k/edit?gid=1074822823#gid=1074822823
     readonly Statistics[] validSACombinationH2 = {
-            new Statistics(0, 1, 0, 0, 1, 2, 0, 0),
-            new Statistics(0, 1, 0, 0, 0, 5, 0, 0),
-            new Statistics(0, 1, 0, 0, 0, 2, 0, 1),
-            new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
-            new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
-            new Statistics(0, 0, 0, 1, 1, 0, 0, 1),
-            new Statistics(0, 0, 0, 1, 0, 6, 0, 0),
-            new Statistics(0, 0, 0, 1, 0, 3, 0, 1),
-            new Statistics(0, 0, 0, 1, 0, 0, 1, 0),
-            new Statistics(0, 0, 0, 1, 0, 0, 0, 2),
-            new Statistics(0, 0, 0, 0, 1, 0, 0, 1),
-            new Statistics(1, 1, 1, 0, 0, 2, 0, 0),
-            new Statistics(1, 1, 0, 0, 1, 0, 0, 0),
-            new Statistics(1, 1, 0, 0, 0, 3, 0, 0),
-            new Statistics(1, 1, 0, 0, 0, 0, 0, 1),
-            new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
-            new Statistics(1, 0, 1, 1, 0, 3, 0, 0),
-            new Statistics(1, 0, 1, 1, 0, 0, 0, 1),
-            new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
-            new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
-            new Statistics(1, 0, 0, 1, 0, 1, 0, 1),
-            new Statistics(1, 0, 0, 0, 1, 1, 0, 0),
-            new Statistics(2, 1, 1, 0, 0, 0, 0, 0),
-            new Statistics(2, 1, 0, 0, 0, 1, 0, 0),
-            new Statistics(2, 0, 2, 1, 0, 0, 0, 0),
-            new Statistics(2, 0, 1, 1, 0, 1, 0, 0),
-            new Statistics(3, 0, 0, 1, 0, 0, 0, 0)
-        };
+        new Statistics(0, 1, 0, 0, 1, 2, 0, 0),
+        new Statistics(0, 1, 0, 0, 0, 5, 0, 0),
+        new Statistics(0, 1, 0, 0, 0, 2, 0, 1),
+        new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
+        new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
+        new Statistics(0, 0, 0, 1, 1, 0, 0, 1),
+        new Statistics(0, 0, 0, 1, 0, 6, 0, 0),
+        new Statistics(0, 0, 0, 1, 0, 3, 0, 1),
+        new Statistics(0, 0, 0, 1, 0, 0, 1, 0),
+        new Statistics(0, 0, 0, 1, 0, 0, 0, 2),
+        new Statistics(0, 0, 0, 0, 1, 0, 0, 1),
+        new Statistics(1, 1, 1, 0, 0, 2, 0, 0),
+        new Statistics(1, 1, 0, 0, 1, 0, 0, 0),
+        new Statistics(1, 1, 0, 0, 0, 3, 0, 0),
+        new Statistics(1, 1, 0, 0, 0, 0, 0, 1),
+        new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
+        new Statistics(1, 0, 1, 1, 0, 3, 0, 0),
+        new Statistics(1, 0, 1, 1, 0, 0, 0, 1),
+        new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
+        new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
+        new Statistics(1, 0, 0, 1, 0, 1, 0, 1),
+        new Statistics(1, 0, 0, 0, 1, 1, 0, 0),
+        new Statistics(2, 1, 1, 0, 0, 0, 0, 0),
+        new Statistics(2, 1, 0, 0, 0, 1, 0, 0),
+        new Statistics(2, 0, 2, 1, 0, 0, 0, 0),
+        new Statistics(2, 0, 1, 1, 0, 1, 0, 0),
+        new Statistics(3, 0, 0, 1, 0, 0, 0, 0)
+    };
 
     // All the possible Silent Assassin combinations for Hitman Contracts
     // https://docs.google.com/spreadsheets/d/1JgNscwEak6pR5qMcUzjRlGh34IG4aZJ6id9V8rahL18/edit?gid=1089548412#gid=1089548412
     readonly Statistics[] validSACombinationHC = {
-            new Statistics(999, 0, 999, 1, 0, 0, 0, 0),
-            new Statistics(2, 1, 1, 0, 0, 0, 0, 0),
-            new Statistics(2, 1, 0, 0, 0, 1, 0, 0),
-            new Statistics(2, 0, 1, 1, 0, 1, 0, 0),
-            new Statistics(2, 0, 0, 0, 0, 2, 0, 0),
-            new Statistics(1, 1, 1, 0, 0, 2, 0, 0),
-            new Statistics(1, 1, 0, 0, 1, 0, 0, 0),
-            new Statistics(1, 1, 0, 0, 0, 3, 0, 0),
-            new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
-            new Statistics(1, 0, 1, 1, 0, 3, 0, 0),
-            new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
-            new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
-            new Statistics(0, 1, 0, 0, 1, 2, 0, 0),
-            new Statistics(0, 1, 0, 0, 0, 5, 0, 0),
-            new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
-            new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
-            new Statistics(0, 0, 0, 1, 0, 6, 0, 0)
-        };
+        new Statistics(999, 0, 999, 1, 0, 0, 0, 0),
+        new Statistics(2, 1, 1, 0, 0, 0, 0, 0),
+        new Statistics(2, 1, 0, 0, 0, 1, 0, 0),
+        new Statistics(2, 0, 1, 1, 0, 1, 0, 0),
+        new Statistics(2, 0, 0, 0, 0, 2, 0, 0),
+        new Statistics(1, 1, 1, 0, 0, 2, 0, 0),
+        new Statistics(1, 1, 0, 0, 1, 0, 0, 0),
+        new Statistics(1, 1, 0, 0, 0, 3, 0, 0),
+        new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
+        new Statistics(1, 0, 1, 1, 0, 3, 0, 0),
+        new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
+        new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
+        new Statistics(0, 1, 0, 0, 1, 2, 0, 0),
+        new Statistics(0, 1, 0, 0, 0, 5, 0, 0),
+        new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
+        new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
+        new Statistics(0, 0, 0, 1, 0, 6, 0, 0)
+    };
     readonly Statistics[] validSACombinationHCMap1 = {
-            new Statistics(999, 0, 0, 1, 0, 0, 0, 0),
-            new Statistics(2, 0, 0, 0, 0, 2, 0, 0),
-            new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
-            new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
-            new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
-            new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
-            new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
-            new Statistics(0, 0, 0, 1, 0, 6, 0, 0)
-        };
+        new Statistics(999, 0, 0, 1, 0, 0, 0, 0),
+        new Statistics(2, 0, 0, 0, 0, 2, 0, 0),
+        new Statistics(1, 0, 1, 1, 1, 0, 0, 0),
+        new Statistics(1, 0, 0, 1, 1, 1, 0, 0),
+        new Statistics(1, 0, 0, 1, 0, 4, 0, 0),
+        new Statistics(0, 0, 0, 1, 1, 3, 0, 0),
+        new Statistics(0, 0, 0, 1, 2, 0, 0, 0),
+        new Statistics(0, 0, 0, 1, 0, 6, 0, 0)
+    };
 
     public Statistics(int sf, int ce, int hs, int al, int ek, int eh, int ik, int ih)
     {
